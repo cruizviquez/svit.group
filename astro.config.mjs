bc8609de567249104a27869f7d535eb525328286
Dr. Carlos Ruiz Viquez <110370052+cruizviquez@users.noreply.github.com>
2026-01-14 07:01:16 +0000

    Adding styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
   site: 'https://svit.group',
   base: '/',
   integrations: [react()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
